7813dbe8eece023c81344aa66ff724b70ccf55d2
Pau <gostassonpauline@gmail.com>
2023-10-25 12:55:55 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=g')
+      .then(response => response.json())
+      .then(data => {
+        const filteredMeals = data.meals.map(meal => {
+          const { strMeal, ...ingredientsAndQuantities } = meal;
+          const ingredients = Object.keys(ingredientsAndQuantities)
+            .filter(key => key.includes('strIngredient'))
+            .map(ingredientKey => {
+              const ingredientIndex = ingredientKey.slice(-1);
+              const quantityKey = `strMeasure${ingredientIndex}`;
+              const name = meal[ingredientKey];
+              const quantity = meal[quantityKey];
+              return { name, quantity };
+            })
+            .filter(({ name }) => name);
+          return { strMeal, ingredients };
+        });
+        setMeals(filteredMeals);
+      })
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  const containerStyle = {
+    fontFamily: 'Arial, sans-serif',
+    maxWidth: '600px',
+    margin: '0 auto',
+    padding: '20px',
+  };
+
+  const mealStyle = {
+    background: '#f0f0f0',
+    padding: '10px',
+    marginBottom: '10px',
+    border: '1px solid #ddd',
+    borderRadius: '5px',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={containerStyle}>
+      <h1 style={{ textAlign: 'center' }}>Meals starting with 'g'</h1>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        {meals.map(({ strMeal, ingredients }) => (
+          <li key={strMeal} style={mealStyle}>
+            <h2>{strMeal}</h2>
+            <ul>
+              {ingredients.map(({ name, quantity }) => (
+                <li key={name}>
+                  {name} - {quantity}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
